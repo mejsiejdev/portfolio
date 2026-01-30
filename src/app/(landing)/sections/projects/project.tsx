f01d0a4dd0b0958de5f8ea5d5a5dbcc7ffbcc0a9
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2026-01-31 00:01:27 +0100

    fix: update project image srcs

--- src/app/(landing)/sections/projects/project.tsx
+++ src/app/(landing)/sections/projects/project.tsx
@@ -38,18 +38,18 @@ export function Project({
     >
       <div className="relative aspect-video w-full overflow-hidden rounded-lg bg-neutral-100 dark:bg-neutral-700">
         <Image
-          src={`${API_URL}/${project.image}`}
+          src={`${API_URL}${project.image}`}
           alt={project.name}
           className="object-cover dark:hidden"
           fill
-          //sizes="(max-width: 768px) 100vw, 50vw"
+          sizes="(max-width: 768px) 100vw, 50vw"
         />
         <Image
-          src={`${API_URL}/${project.image_dark ? project.image_dark : project.image}`}
+          src={`${API_URL}${project.image_dark ? project.image_dark : project.image}`}
           alt={project.name}
           className="hidden object-cover dark:block"
           fill
-          //sizes="(max-width: 768px) 100vw, 50vw"
+          sizes="(max-width: 768px) 100vw, 50vw"
         />
       </div>
       <div className="space-y-2">
